7000d1dd57242c19982c3a6773987b719aacae17
zapisochnyivv <zapisochnyivv@elocont.ru>
2025-07-29 01:36:36 +0300

    HamsterTimer flow excerpt implemented

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
+	kotlin("jvm") version "2.0.0"
+	kotlin("plugin.spring") version "2.0.0"
 	id("org.springframework.boot") version "3.5.4"
 	id("io.spring.dependency-management") version "1.1.7"
+	application
 }
 
 group = "mel.wadzapi.interview"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+	sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
@@ -21,17 +20,17 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+	implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
-}
-
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+application {
+	mainClass.set("mel.wadzapi.interview.HammelCounterApplication")
 }
--- src/main/kotlin/mel/wadzapi/interview/service/HamStatService.kt
+++ src/main/kotlin/mel/wadzapi/interview/service/HamStatService.kt
@@ -1,19 +1,22 @@
-package mel.wadzapi.interview.service.impl
+package mel.wadzapi.interview.service
 
 import mel.wadzapi.interview.controller.dto.EventHappensDto
 import mel.wadzapi.interview.controller.dto.HamStatResponseDto
-import mel.wadzapi.interview.service.StatTimerService
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import java.time.Instant
 import java.time.temporal.ChronoUnit
-import java.util.*
-import java.util.Collections.emptySet
+import java.util.Collections
+import java.util.HashMap
+import java.util.HashSet
+import java.util.Objects
 import java.util.concurrent.ConcurrentHashMap
 import java.util.stream.IntStream
 
 @Service
-class HamStatService : StatTimerService {
-    private val userStat: Map<Instant, MutableMap<Int, MutableSet<Int>>> = ConcurrentHashMap()
+class HamStatService(ham: HamsterTimer) : StatTimerService {
+    val hamTimer: HamsterTimer = ham
+    private val userStat: MutableMap<Instant, MutableMap<Int, MutableSet<Int>>> = ConcurrentHashMap()
     override fun today(): Instant {
         return Instant.now().truncatedTo(ChronoUnit.DAYS)
     }
@@ -29,8 +32,14 @@ class HamStatService : StatTimerService {
             userStat[today().minus(1, ChronoUnit.DAYS)]
         if (Objects.isNull(lastDayMap)) return HamStatResponseDto(IntArray(0))
         val cnt = IntStream.of(*hamsterIds)
-            .map { hamId: Int -> (lastDayMap!![hamId] ?: emptySet()).size }
+            .map { hamId: Int -> (lastDayMap!![hamId] ?: Collections.emptySet()).size }
             .toArray()
         return HamStatResponseDto(cnt)
+    }
+
+    @Scheduled(cron = "0 10 0 * * *", zone = "UTC")
+    internal suspend fun statClean() {
+        userStat.remove(today().minus(2, ChronoUnit.DAYS))
+        hamTimer.flowEventRandomized()
     }
 }
